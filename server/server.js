ac0cf9a7ef5158a828a0ad38be9eb5486a531966
Gurjinder Singh <rockygsingh@icloud.com>
2022-06-20 18:56:16 -0400

    wrote a route for getting all the restaurants

--- server/server.js
+++ server/server.js
@@ -4,6 +4,20 @@ const express = require('express');
 
 const app = express();
 
+
+
+/* GETS ALL THE RESTAURANTS */
+app.get('/api/v1/restaurants', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            restaurants: ['pizza', 'wendys']
+        }
+    })
+})
+
+
+
 const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server is up and listening on port ${port}`));
