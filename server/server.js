882a210ff5ee541e1e506f427861075a484ec78b
Gurjinder Singh <rockygsingh@icloud.com>
2022-06-26 06:46:44 -0400

    created get and post routes in the api

--- server/server.js
+++ server/server.js
@@ -5,11 +5,12 @@ const db = require("./db");
 
 const app = express();
 
-/* parse the json in the body of the req */
+/* parse the json in the body of the req, the json object coming from client */
 app.use(express.json());
 
 /* GETS ALL THE RESTAURANTS */
 app.get("/api/v1/restaurants", async (req, res) => {
+
   try {
     /* get the data from the db */
     const results = await db.query("select * from restaurants");
@@ -29,11 +30,46 @@ app.get("/api/v1/restaurants", async (req, res) => {
   }
 });
 
-/* Get a Restaurant */
-app.get("/api/v1/restaurants/:id", (req, res) => {});
+/* Get a Single Restaurant using params id */
+app.get("/api/v1/restaurants/:id", async (req, res) => {
 
-app.post("/api/v1/restaurants", (req, res) => {
+  try {
+    const results = await db.query(
+      "select * from restaurants where id = $1", [req.params.id]);
+
+    res.status(200).json({
+      status: 'succes',
+      data: {
+        restaurant: results.rows[0],
+      }
+    })
+  } catch (err) {
+    console.log(`this error is from single restaurant route ${err}`);
+  }
+ 
+});
+
+
+/* Create a Restaurant  */
+app.post("/api/v1/restaurants", async (req, res) => {
   console.log(req.body);
+
+  try {
+    /* returning * tells postpg to return the data that was posted */
+    const results = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
+      [req.body.name, req.body.location, req.body.price_range] );
+      console.log(results)
+
+      res.status(201).json({
+        status: 'succes',
+        data: {
+          restaurant: results.rows[0]
+        }
+      })
+  } catch (err) {
+    console.log(`Error from post a restaurant ${err}`);
+  }
 });
 
 /* Update Restaurants */
