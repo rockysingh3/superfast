cfdd783bb5b3992d0aada94bb5503d7cc25362a7
Gurjinder Singh <rockygsingh@icloud.com>
2022-06-26 06:46:18 -0400

    connected to the db

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg')
 
-const DBdata = require('./dbAuth')
+// const DBdata = require('./dbAuth')
 
-const pool = new Pool(
-    DBdata.data
-)
+const pool = new Pool()
+
+
+// you can also use async/await
 
 module.exports = {
-    query: (text, params) => pool.query(text, params)
+    query: (text, params) => pool.query(text, params),
 }
